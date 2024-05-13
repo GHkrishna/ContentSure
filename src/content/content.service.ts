427946a2bf23942f83dad58433e288eabf037e77
Krishna <krishna.waske@ayanworks.com>
2024-05-13 07:58:58 +0530

    update: Logic to sign content with custom manifest and provide caching
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -1,21 +1,27 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { AxiosRequestConfig } from 'axios';
-import { FileUploadDto } from './content.dto';
+import { manifest } from '../commons/manifest';
+
 import * as fs from 'fs';
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import { join } from 'path';
+import { IFileUpload } from './content.interface';
+import { CACHE_MANAGER, CacheStore } from '@nestjs/cache-manager';
 
 @Injectable()
 export class ContentService {
   private readonly logger = new Logger(ContentService.name);
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheManager: CacheStore,
+  ) {}
 
   private token: string = '';
 
-  async signContent(body: FileUploadDto): Promise<any> {
+  async signContent(body: IFileUpload): Promise<any> {
     console.log('entered service');
     // get token (from cache or if expired make a call and get token)
     // take content and pass it to sign
@@ -37,40 +43,61 @@ export class ContentService {
         /^data:image\/[A-Za-z-+/]+;base64,/,
         '',
       );
+
       const binaryData = Buffer.from(base64Image, 'base64');
       const filename = `image_${Date.now()}.${imageType}`;
       const inputFilePath = `uploadedFiles/${filename}`;
       const outputFilePath = `uploadedSignedFiles/${filename}`;
+      const manifestFile = manifest;
+
+      // Create a custom assertiona and manifest for each request
+      manifestFile.title = body.title;
+      manifestFile.assertions[1].data.author[0].name = body.authorName;
+      manifestFile.assertions[1].data.author[1]['@id'] = body.authorSocial;
+      manifestFile.assertions[1].data.author[1].name = body.authorName;
+
+      // Make the manifest file compatible in cli
+      const strManifest = JSON.stringify(manifestFile).replace(/'/g, "\\'");
+
+      console.log('This is stringified JSON::::', strManifest);
 
       console.log('This is the binaryData:::', binaryData);
       console.log('This is the filename:::', filename);
       console.log('This is the inputFilePath:::', inputFilePath);
       console.log('This is the outputFilePath:::', outputFilePath);
 
-      // Write binary data to a file
-      // fs.writeFileSync(inputFilePath, binaryData);
-
       const fsPromises = fs.promises;
       await fsPromises.appendFile(inputFilePath, binaryData, {
         flag: 'w',
       });
-      const command = `./c2patool ${inputFilePath} -m updatedManifest.json -o ${outputFilePath} -f`;
+      // const command = `./c2patool ${inputFilePath} -m updatedManifest.json -o ${outputFilePath} -f`;
 
-      // To add custom JSON:
-      // const updatedCommand = `c2patool sample/image.jpg \
-      // -c '{"ta_url": "http://timestamp.digicert.com", "claim_generator": "CAI_Demo/0.1", "title": "", "assertions": [{"label": "c2pa.actions", "data": {"actions": [{"action": "c2pa.published"}]}}]}'`;
+      // Command to sign the image from 'inputFilePath' with manifest 'strManifest' and store it in 'outputFilePath'
+      const command = `./c2patool ${inputFilePath}  \ -c '${strManifest}' -o ${outputFilePath} -f`;
 
       const { stdout, stderr } = await exec(command);
 
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
+      // Read from the signed file
       const readFile = promisify(fs.readFile);
       const imageBuffer = await readFile(join(process.cwd(), outputFilePath));
       const base64 = imageBuffer.toString('base64');
+      // Convert the file metadata into valid base64 url
       const imageUrl = `data:image/${imageType};base64,${base64}`;
 
       console.log('This is the stdout:::', stdout);
       console.log('This is the stderr:::', stderr);
+
+      if (body.authorEmail) {
+        // If authemail present, send a credential to author about publication
+        await this.sendCredentialToAuthor(
+          body.authorEmail,
+          body.authorName,
+          body.publishedBy,
+          body.title,
+        );
+      }
 
       return imageUrl;
     } catch (err) {
@@ -79,6 +106,30 @@ export class ContentService {
     }
   }
 
+  // Send credential to author
+  async sendCredentialToAuthor(
+    authorEmail: string,
+    author: string,
+    publishedBy: string,
+    title: string,
+  ) {
+    console.log('This is authorEmail::::::', authorEmail);
+    console.log('This is author::::::', author);
+    console.log('This is publishedBy::::::', publishedBy);
+    console.log('This is title::::::', title);
+
+    let token = await this.cacheManager.get('token');
+
+    if (!token) {
+      token = await this.getToken();
+      await this.cacheManager.set('token', token, 1000);
+      console.log('this is token inside if condition:::', token);
+    }
+
+    console.log('this is token:::', token);
+  }
+
+  // Get token required to make API calls to credebl
   async getToken() {
     try {
       if (this.token !== '') {
