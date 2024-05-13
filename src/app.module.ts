3501202318eb4f63c8cee87f5e52daba2bd27683
Krishna <krishna.waske@ayanworks.com>
2024-05-13 07:56:49 +0530

    add: Cache module
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,23 @@ import { AuthService } from './auth/auth.service';
 import { HttpModule } from '@nestjs/axios';
 import { ContentModule } from './content/content.module';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [HttpModule, ContentModule, ConfigModule.forRoot()],
-  controllers: [AppController, AuthController, ContentController],
+  imports: [
+    HttpModule,
+    ContentModule,
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [
+    AppController,
+    AuthController,
+    ContentController,
+    AppController,
+  ],
   providers: [AppService, ContentService, AuthService],
 })
 export class AppModule {}
